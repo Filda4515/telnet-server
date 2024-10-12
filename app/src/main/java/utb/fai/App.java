b38854a388cb07aa06acdc829d0002bddc35be7f
Filda4515 <48486481+Filda4515@users.noreply.github.com>
2024-10-12 20:04:04 +0200

    Executor implementation

--- app/src/main/java/utb/fai/App.java
+++ app/src/main/java/utb/fai/App.java
@@ -1,16 +1,29 @@
 package utb.fai;
 
+import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class App {
 
     public static void main(String[] args) {
-        int port = 12345;
-        int max_threads = 10;    
+        int port = Integer.parseInt(args[0]);
+        int max_threads = Integer.parseInt(args[1]);
 
-        // Implement input parameter processing
-        
-        // Implementation of the main server loop
-        ServerSocket serverSocket;
+        ExecutorService executor = Executors.newFixedThreadPool(max_threads);;
+        Socket clientSocket;
+
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            while (true) {
+                clientSocket = serverSocket.accept();
+                executor.execute(new ClientThread(clientSocket));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            executor.shutdown();
+        }
     }
 }

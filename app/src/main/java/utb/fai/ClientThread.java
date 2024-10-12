f2280c6acb3b64105c17b4f87310303184a7b722
Filda4515 <48486481+Filda4515@users.noreply.github.com>
2024-10-12 20:01:25 +0200

    Telnet echo server implementation

--- app/src/main/java/utb/fai/ClientThread.java
+++ app/src/main/java/utb/fai/ClientThread.java
@@ -13,6 +13,22 @@ public class ClientThread extends Thread {
 
     @Override
     public void run() {
-        // Implementation of processing incoming communication from the telnet client
+        try (
+            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true)
+        ) {
+            String clientMessage;
+            while ((clientMessage = reader.readLine()) != null) {
+                writer.println(clientMessage);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                clientSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
